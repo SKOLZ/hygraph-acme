b4c8f4a363dd7c946caaeb790bc15dd463996072
SKOLZ <gabriel.zanzotti@gmail.com>
2024-03-02 16:05:37 +0100

    implemented the desktop version for the callout block.

--- src/components/Block/components/Callout/index.tsx
+++ src/components/Block/components/Callout/index.tsx
@@ -1,7 +1,27 @@
 import { BlockCallout } from "@/generated/graphql";
+import { getButtonSizeClassName } from "@/utils/getButtonSizeClassName";
+import { RichText } from "@graphcms/rich-text-react-renderer";
+import Link from "next/link";
 
 export const Callout: React.FC<BlockCallout> = (callout) => {
   return (
-    <div>Callout</div>
+    <div className="px-8 w-full max-w-screen-xl mx-auto">
+      <div className="p-16 rounded-2xl flex justify-between items-start bg-secondary-800">
+        <div className="flex flex-col items-start gap-4">
+          <h2 className="title-2 text-white">{callout.title}</h2>
+          <RichText
+            content={callout.description.raw}
+            renderers={{
+              p: ({ children }) => (
+                <p className="text-1 text-white">{children}</p>
+              )
+            }}
+          />
+        </div>
+        <Link href="#" className={`button ${getButtonSizeClassName(callout.actionButton.size)}`}>
+          {callout.actionButton.buttonText}
+        </Link>
+      </div>
+    </div>
   );
 } 
